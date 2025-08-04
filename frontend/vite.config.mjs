ee0b334490ece48e0155dae5207672e0b5f31888
MLEE99999 <rs994824@naver.com>
2025-08-04 07:37:11 +0000

    갈아엎기

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -6,19 +6,19 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
-      '@layouts': fileURLToPath(new URL('./src/@layouts', import.meta.url)),
-      '@configured-variables': fileURLToPath(new URL('./src/styles/variables/_template.scss', import.meta.url)),
-      '@axios': fileURLToPath(new URL('./src/plugins/axios', import.meta.url)),
-      '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
-      '@style': fileURLToPath(new URL('./src/styles', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
+  //     '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
+  //     '@layouts': fileURLToPath(new URL('./src/@layouts', import.meta.url)),
+  //     '@configured-variables': fileURLToPath(new URL('./src/styles/variables/_template.scss', import.meta.url)),
+  //     '@axios': fileURLToPath(new URL('./src/plugins/axios', import.meta.url)),
+  //     '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+  //     '@style': fileURLToPath(new URL('./src/styles', import.meta.url)),
+  //     '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+  //     'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
+  //   },
+  // },
   build: {
     chunkSizeWarningLimit: 5000,
   },
